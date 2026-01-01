d2bd72290333e094d47b14dd38481785fa33ef69
Majaahh <maja.blaszczak@proton.me>
2026-01-01 18:02:04 +0100

    a9y18qlte: Namespace the tree
    Change-Id: I1a94f8f38810801879b8545fbf464e730d297c42
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -12,3 +12,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/init/recovery.fstab:recovery/root/system/etc/recovery.fstab \
     $(DEVICE_PATH)/configs/init/twrp.fstab:recovery/root/system/etc/twrp.fstab \
     $(DEVICE_PATH)/configs/init/ueventd.qcom.rc:recovery/root/ueventd.qcom.rc
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
